363d84cd97120dae7303c609c603e57eb0bb4bf7
Eranda Nandasena <erandac@Erandas-MacBook-Pro.local>
2019-09-22 22:28:47 +0530

    merge from original

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,4 @@
 #docker run -e keymap=erandac  -e subproject= -e keyboard=ergodox_ez --rm -v $('pwd'):/qmk:rw edasque/qmk_firmware
+git submodule update --recursive --remote
 util/docker_build.sh ergodox_ez:erandac
 ./teensy_loader_cli -mmcu=atmega32u4 -w -v ergodox_ez_erandac.hex
