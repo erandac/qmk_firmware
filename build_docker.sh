17b3dc64b2690d706efcf89af4bbdd30dcb61827
Eranda Nandasena <erandac@Erandas-MacBook-Pro.local>
2019-09-22 21:28:50 +0530

    using native build utils

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,3 @@
-docker run -e keymap=erandac  -e subproject= -e keyboard=ergodox_ez --rm -v $('pwd'):/qmk:rw edasque/qmk_firmware
+#docker run -e keymap=erandac  -e subproject= -e keyboard=ergodox_ez --rm -v $('pwd'):/qmk:rw edasque/qmk_firmware
+util/docker_build.sh ergodox_ez:erandac
 ./teensy_loader_cli -mmcu=atmega32u4 -w -v ergodox_ez_erandac.hex
